0f4c36a5ea22e6603a52093a3bf89112f9f07ff0
Milton <55441239+jmrchelani@users.noreply.github.com>
2021-02-01 18:11:18 +0500

    Create Collection.java

---
+++ java/Collections/Collection.java
@@ -0,0 +1,8 @@
+public interface Collection {
+    void clear();
+    boolean add(Object object);
+    Iterator iterator();
+    boolean isEmpty();
+    int size();
+    boolean remove(Object object);
+}
